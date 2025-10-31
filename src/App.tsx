133416702cea43435114454a52b04640b9861da9
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-10-31 16:33:53 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import UserRoute from './routes/route'
 import { Toaster } from "sonner";
